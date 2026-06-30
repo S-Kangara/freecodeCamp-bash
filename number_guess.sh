9926e5070f98905cdd51532ad7a794a9c7ed21f1
Sathsarani Kangara <prabodhiyakangara@gmail.com>
2026-06-30 12:10:46 -0400

    feat: complete number guessing game

--- number_guess.sh
+++ number_guess.sh
@@ -2,11 +2,17 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+# -------------------------
+# USERNAME INPUT
+# -------------------------
 echo "Enter your username:"
 read USERNAME
 
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
 
+# -------------------------
+# NEW USER
+# -------------------------
 if [[ -z $USER_ID ]]
 then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
@@ -15,14 +21,23 @@ then
 
   USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
 
+# -------------------------
+# EXISTING USER
+# -------------------------
 else
   GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID;")
-
   BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID;")
+
+  # clean output
+  GAMES_PLAYED=$(echo $GAMES_PLAYED | xargs)
+  BEST_GAME=$(echo $BEST_GAME | xargs)
 
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
+# -------------------------
+# GAME START
+# -------------------------
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 
 echo "Guess the secret number between 1 and 1000:"
@@ -33,7 +48,7 @@ GUESS_COUNT=1
 while [[ $GUESS != $SECRET_NUMBER ]]
 do
   if [[ ! $GUESS =~ ^[0-9]+$ ]]
-  then 
+  then
     echo "That is not an integer, guess again:"
   elif [[ $GUESS -gt $SECRET_NUMBER ]]
   then
@@ -46,6 +61,9 @@ do
   ((GUESS_COUNT++))
 done
 
-echo "You gussed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
+echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
 
+# -------------------------
+# SAVE GAME TO DATABASE
+# -------------------------
 INSERT_GAME=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $GUESS_COUNT);")
